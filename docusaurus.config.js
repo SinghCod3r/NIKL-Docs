97b25877eb850de751d0a624814a5f5b1bd003e6
Neko Nik <nikhil@nekonik.com>
2025-05-18 16:41:55 +0530

    Update Docusaurus configuration and dependencies: set version to 1.0.0 and add Vercel analytics plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,144 +5,153 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'NIKL',
-  tagline: 'Lightweight, concurrent, network-aware programming language',
-  favicon: 'img/favicon.ico',
+    title: 'NIKL',
+    tagline: 'Lightweight, concurrent, network-aware programming language',
+    favicon: 'img/favicon.ico',
 
-  // Set the production URL of your site here
-  url: 'https://nikl.nekonik.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: '/',
+    // Set the production URL of your site here
+    url: 'https://nikl.nekonik.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  organizationName: 'Neko-Nik', // Usually your GitHub org/user name.
-  projectName: 'NIKL-Docs', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+    // GitHub pages deployment config.
+    organizationName: 'Neko-Nik', // Usually your GitHub org/user name.
+    projectName: 'NIKL-Docs', // Usually your repo name.
+    deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          lastVersion: 'current',
-          editUrl: 'https://github.com/Neko-Nik/NIKL-Docs/edit/main/',
-            versions: {
-                current: {
-                    label: 'v0.1 (unreleased)',
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    lastVersion: 'current',
+                    editUrl: 'https://github.com/Neko-Nik/NIKL-Docs/edit/main/',
+                    versions: {
+                        current: {
+                            label: 'v0.1 (unreleased)',
+                        },
+                    },
+                    showLastUpdateAuthor: true,
+                    showLastUpdateTime: true,
                 },
-            },
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'NIKL',
-        logo: {
-          alt: 'NIKL Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
-          },
-          {
-            href: 'https://github.com/Neko-Nik/NIKL-Core',
-            label: 'GitHub',
-            position: 'right',
-          },
-          {
-            type: 'docsVersionDropdown',
-            position: 'left',
-            dropdownActiveClassDisabled: true,
-            // dropdownItemsAfter: [{href: 'https://nikl-pkg.nekonik.com', label: 'Package Manager (WIP)'}],
-          }
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'NIKL',
+                logo: {
+                    alt: 'NIKL Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'tutorialSidebar',
+                        position: 'left',
+                        label: 'Docs',
+                    },
+                    {
+                        href: 'https://github.com/Neko-Nik/NIKL-Core',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                    {
+                        type: 'docsVersionDropdown',
+                        position: 'left',
+                        dropdownActiveClassDisabled: true,
+                        // dropdownItemsAfter: [{href: 'https://nikl-pkg.nekonik.com', label: 'Package Manager (WIP)'}],
+                    }
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Getting Started',
+                                to: '/docs/introduction',
+                            },
+                            {
+                                label: 'NIKL GitHub',
+                                href: 'https://github.com/Neko-Nik/NIKL-Core',
+                            },
+                            {
+                                label: 'Package Manager (WIP)',
+                                href: 'https://nikl-pkg.nekonik.com',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Neko Nik Forums',
+                                href: 'https://forums.nekonik.com',
+                            },
+                            {
+                                label: 'Status Page',
+                                href: 'https://status.nekonik.com',
+                            },
+                            {
+                                label: 'LinkedIn',
+                                href: 'https://www.linkedin.com/in/neko-nik',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'Author Website',
+                                href: 'https://www.nekonik.com',
+                            },
+                            {
+                                label: 'ORCID',
+                                href: 'https://orcid.org/0009-0008-3435-5674',
+                            },
+                            {
+                                label: 'GitHub Profile',
+                                href: 'https://github.com/Neko-Nik',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `© 2025 Neko Nik. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+        }),
+    plugins: [
+        [
+            'vercel-analytics',
+            {
+                debug: true,
+                mode: 'auto',
+            },
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-            {
-            title: 'Docs',
-            items: [
-                {
-                label: 'Getting Started',
-                to: '/docs/introduction',
-                },
-                {
-                label: 'NIKL GitHub',
-                href: 'https://github.com/Neko-Nik/NIKL-Core',
-                },
-                {
-                label: 'Package Manager (WIP)',
-                href: 'https://nikl-pkg.nekonik.com',
-                },
-            ],
-            },
-            {
-            title: 'Community',
-            items: [
-                {
-                label: 'Neko Nik Forums',
-                href: 'https://forums.nekonik.com',
-                },
-                {
-                label: 'Status Page',
-                href: 'https://status.nekonik.com',
-                },
-                {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/in/neko-nik',
-                },
-            ],
-            },
-            {
-            title: 'More',
-            items: [
-                {
-                label: 'Author Website',
-                href: 'https://www.nekonik.com',
-                },
-                {
-                label: 'ORCID',
-                href: 'https://orcid.org/0009-0008-3435-5674',
-                },
-                {
-                label: 'GitHub Profile',
-                href: 'https://github.com/Neko-Nik',
-                },
-            ],
-            },
-        ],
-        copyright: `© 2025 Neko Nik. Built with Docusaurus.`,
-        },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+    ],
 };
 
 export default config;
